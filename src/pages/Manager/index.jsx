064338544f1a0735a4a277ec3c6b85ae769810be
Gnauqoa <quanglng2022@gmail.com>
2023-06-25 23:58:44 +0700

    add protext

--- src/pages/Manager/index.jsx
+++ src/pages/Manager/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AddExamInfo from "./AddExamInfo";
 import AddPointInfo from "./AddPointInfo";
 import { useSelector } from "react-redux";
@@ -10,17 +10,22 @@ import { getStudentRegister } from "services/auth";
 const Manager = () => {
   const studentRegisterRequest = useAPI({
     queryFn: getStudentRegister,
-    getNow: true,
   });
+  const loginStatus = useSelector((state) => state.loginStatus);
+
+  useEffect(() => {
+    if (loginStatus.isLogin) studentRegisterRequest.run();
+  }, [loginStatus]);
   const email_list = studentRegisterRequest?.response?.map(
     (student) => student.email
   );
-  const loginStatus = useSelector((state) => state.loginStatus);
+
   if (loginStatus.isChecking) return <CircularProgress />;
   if (!loginStatus.isLogin) {
     console.log("out e");
     return <Navigate to="/auth/login" />;
   }
+
   return (
     <div className="flex flex-col p-10 gap-7">
       <AddExamInfo
